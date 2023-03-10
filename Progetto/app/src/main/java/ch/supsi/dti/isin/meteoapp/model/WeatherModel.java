90906f075786c739770bc85409b320bf71aa281c
Jari Näser <jari.naeser@supsi.ch>
2023-03-10 19:09:22 +0100

    Update WeatherModel.java

--- Progetto/app/src/main/java/ch/supsi/dti/isin/meteoapp/model/WeatherModel.java
+++ Progetto/app/src/main/java/ch/supsi/dti/isin/meteoapp/model/WeatherModel.java
@@ -4,6 +4,7 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 public class WeatherModel {
+
     private JSONObject requestWeather(String location){
         JSONObject response = null;
         try{
@@ -12,6 +13,8 @@ public class WeatherModel {
             // Set main.temp to Metric, otherwise temnperature will be in Kelvin!!
 
             response = new JSONObject("{\"coord\":{\"lon\":-0.13,\"lat\":51.51},\"weather\":[{\"id\":300,\"main\":\"Drizzle\",\"description\":\"light intensity drizzle\",\"icon\":\"09d\"}],\"base\":\"stations\",\"main\":{\"temp\":280.32,\"pressure\":1012,\"humidity\":81,\"temp_min\":279.15,\"temp_max\":281.15},\"visibility\":10000,\"wind\":{\"speed\":4.1,\"deg\":80},\"clouds\":{\"all\":90},\"dt\":1485789600,\"sys\":{\"type\":1,\"id\":5091,\"message\":0.0103,\"country\":\"GB\",\"sunrise\":1485762037,\"sunset\":1485794875},\"id\":2643743,\"name\":\"London\",\"cod\":200}\n");
+
+            //System.out.println(response.getJSONObject("main").);
         }catch(JSONException jsone){
             System.err.println("Error while requesting JSON.\n" + jsone.getMessage());
         }
@@ -23,11 +26,20 @@ public class WeatherModel {
         if(weather == null) return null;
 
         return new WeatherCondition(
-                (int) weather.getJSONObject("weather").get("id"),
-                weather.getJSONObject("weather").get("main").toString(),
-                weather.getJSONObject("weather").get("description").toString(),
-                weather.getJSONObject("weather").get("icon").toString(),
-                (float) weather.getJSONObject("main").get("temp")
+                weather.getJSONObject("weather").getInt("id"),
+                weather.getJSONObject("weather").getString("main"),
+                weather.getJSONObject("weather").getString("description"),
+                weather.getJSONObject("weather").getString("icon"),
+                weather.getJSONObject("main").getDouble("temp"),
+                weather.getJSONObject("main").getInt("pressure"),
+                weather.getJSONObject("main").getInt("humidity"),
+                weather.getJSONObject("main").getDouble("temp_min"),
+                weather.getJSONObject("main").getDouble("temp_max"),
+                weather.getJSONObject("name").toString(),
+                weather.getJSONObject("sys").getString("country"),
+                weather.getJSONObject("sys").getLong("sunrise"),
+                weather.getJSONObject("sys").getLong("sunset"),
+                weather.getJSONObject("wind").getDouble("speed")
         );
     }
 
